a05f4dd2b128eb1f5f1ad3629c51cfaccad19feb
Suvodeep Sinha <suvodeepsinha1981@gmail.com>
2021-06-05 18:12:48 +0530

    started vis webdocs

---
+++ Visualization/Input/webdocs(10-20).py
@@ -0,0 +1,30 @@
+
+grp_id =''
+attr = []
+with open('Visualization\Input\wdemo.txt') as file:
+    for line in file:
+        line = line.strip()
+        if line[0] == '"':
+            grp_id = line
+        else:
+            for i in range(len(line)):
+                if line[i] == '.':
+                    attr.append(float(line[i-1:]))
+                    
+
+# for 2 digit
+
+start = int(grp_id[-5:-3])
+end = int(grp_id[-2:])
+gid = grp_id[1:-7]
+
+per = 0.5
+cou = 0
+
+for i in attr:
+    if i > per:
+        # print(i)
+        cou += 1
+print(cou)
+
+
