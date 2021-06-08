79fdfe31c569a8f6b61acbcf9b1f935ae5c1bfb1
Suvodeep Sinha <suvodeepsinha1981@gmail.com>
2021-06-08 19:22:05 +0530

    webdocs(10-20) 60 per done

--- Visualization/Input/webdocs(10-20).py
+++ Visualization/Input/webdocs(10-20).py
@@ -18,13 +18,18 @@ start = int(grp_id[-5:-3])
 end = int(grp_id[-2:])
 gid = grp_id[1:-7]
 
-per = 0.5
+per = 0.6
 cou = 0
 
 for i in attr:
-    if i > per:
+    if i >=per:
         # print(i)
         cou += 1
 print(cou)
+
+f = open('Visualization/Output/webdocs(10-20).txt', 'a')
+f.write(str(cou))
+f.write(" ")
+
 
 
