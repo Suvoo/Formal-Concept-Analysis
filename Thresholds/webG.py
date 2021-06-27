85b79a94593f28f5d97d973ce84fc64ce3822beb
Suvodeep Sinha <suvodeepsinha1981@gmail.com>
2021-06-27 17:51:18 +0530

    completed threshold for Webdocs

---
+++ Thresholds/webG.py
@@ -0,0 +1,52 @@
+threshold = 423089
+theta = 846041
+a,brr=[],[]
+ac=''
+with open('Thresholds\webdocs_Support') as file:
+    for line in file:
+        line = line.strip()
+        line = line + ' '
+
+        if(line[0] != '"'):
+            for c in line:
+                if c != ' ':
+                    ac = ac + c
+                else:
+                    a.append(int(ac))
+                    # print(ac)
+                    ac=''
+            brr.append(a)
+            a=[]
+    
+
+# print(brr)
+# "Supp of the Att" "Att ID"
+
+# tindex is if arr[i][0] == threshold - unique for all
+arr = []
+tindex = 28
+for i in range(len(brr)):
+    if i >= tindex:
+        arr.append(brr[i])
+# print(arr)
+
+sgr = 0
+ans = []
+
+begin = arr[77][0]
+to_append = arr[77][1]
+
+ans.append(to_append)
+
+for i in range(len(arr)-155,-1,-1): # minus lenght from rear
+    sgr = sgr + arr[i][0]
+    # print(sgr)
+    if(sgr + begin >= theta):
+        break
+    else:
+        ans.append(arr[i][1])
+print(ans)
+
+f = open('Thresholds/grWeb', 'a')
+f.write(str(ans))
+f.write(" ")
